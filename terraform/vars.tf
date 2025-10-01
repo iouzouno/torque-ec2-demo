f134ff8d57e813deb713ecda52f04596ba17f6c0
iouzouno <89134918+iouzouno@users.noreply.github.com>
2025-10-01 19:39:08 +0300

    Initial commit of torque test

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -26,9 +26,6 @@ variable "USER" {
   default = "ubuntu"
 }
 
-variable "PUB_KEY" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCHFXlATI6c26UN/IZSV6XuvQ+ADvLevXlmGgoNb3OBhld6ZP1G7KfUjgJmKjy+Icws4hgX8kwcZu/ERRy9IYOFvqXXt3p0QTX/VJGGzgs8dr2lBOAthy2iQcwkxpWbrRVzZu8i17aN8GXOvz8BVT0xDvMW5suP5humuQ+DlXJIDwoSjhU5t1+vkvFhaZKwHfbj7kykpay168oMIBOoj0zgs53MzNnR3llNWPIZHGTPSS4gW2xWWdGCFU1AwlrtVDE/35Vs+OOE3pe/h0IIHCZk0tmGc64o5xxXMZEfInlxH1E+qaWntl3Ao9Z3qUMkzFNvZRqY+zatD1kRCST/glY7"
-}
 
 
 
